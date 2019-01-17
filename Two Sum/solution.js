dc761c2dbeb96344e355dd8b4b1c8bd47866ea54
PRITAM <pritam.das@mail.utoronto.ca>
2019-01-16 21:51:20 -0500

    Added an alternate version using just one loop

--- Two Sum/solution.js
+++ Two Sum/solution.js
@@ -6,3 +6,14 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+
+//ALTERNATE
+
+var twoSum = function(numbers, target) {
+  var l=numbers.length, i=0, j=l-1;
+  while (numbers[i]+numbers[j] !== target) {
+    numbers[i]+numbers[j] < target ? i++ : j--;
+  }
+  return [i+1, j+1];
+};
