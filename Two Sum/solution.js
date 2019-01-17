43b5dc7e831743d3bff720fc7340813b9af63270
PRITAM <pritam.das@mail.utoronto.ca>
2019-01-16 21:50:32 -0500

    Solution to two sum using nested fors

---
+++ Two Sum/solution.js
@@ -0,0 +1,8 @@
+var twoSum = function(numbers, target) {
+    for(var i=0; i<numbers.length; i++){
+
+        for(var j=(i+1); j<numbers.length; j++){
+            if(numbers[i] + numbers[j] === target) return [i+1,j+1];
+        }
+    }
+};
